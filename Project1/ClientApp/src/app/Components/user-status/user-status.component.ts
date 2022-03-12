d553abad7307e1d1987198461e4232647c23d31c
zagan.paul.andrei <zagan.paul.andrei@gmail.com>
2022-03-12 23:22:50 +0200

    Added FiltredUserList changed userStarus interface because json names were lowercase

--- Project1/ClientApp/src/app/Components/user-status/user-status.component.ts
+++ Project1/ClientApp/src/app/Components/user-status/user-status.component.ts
@@ -11,28 +11,40 @@ export class UserStatusComponent implements OnInit {
 
   public userStatus: UserStatus[];
   selectedUser?: UserStatus;
+  public filtredUserList: UserStatus[];
 
   /** UserStatus ctor */
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<UserStatus[]>(baseUrl + 'alluser/').subscribe(result => {
       this.userStatus = result;
-      console.log(result);
     }, error => console.error(error));
     console.log(baseUrl);
+
+    //Scoate asta mai tarziu
+    this.filtredUserList = this.userStatus;
+    //console.log(this.userStatus);
+    //
   }
 
   ngOnInit(): void {}
 
   onSelect(user: UserStatus): void {
     this.selectedUser = user;
+
+  }
+  fliter(users: UserStatus): void {
+    //someLogicHere
+    this.filtredUserList = this.userStatus;
   }
 }
 
+
 interface UserStatus {
-  Name: string;
-  Surname: string;
-  BuildingName: string;
-  Office: string;
-  RemoteStatus: string;
+  id: number;
+  name: string;
+  surname: string;
+  buildingName: string;
+  office: string;
+  remoteStatus: string;
 }
 
